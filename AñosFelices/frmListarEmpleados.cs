484b82564bffdc9eb5e5add51b7d50d662cd690f
MaVarela <32914499+MaVarela@users.noreply.github.com>
2017-11-10 22:15:02 -0300

    MergeJuanAyeLore

---
+++ AñosFelices/frmListarEmpleados.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AñosFelices.AccesoADatos.IRepositorios;
+using AñosFelices.AccesoADatos.Repositorios;
+
+namespace AñosFelices
+{
+    public partial class frmListarEmpleados : Form
+    {
+        IRepositorioUsuario repositorioUsuario = new RepositorioUsuario();
+
+        public frmListarEmpleados()
+        {
+            InitializeComponent();
+            this.dgvEmpleados.DataSource = repositorioUsuario.ObtenerTodos();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ AñosFelices/frmRegistrarEmpleado.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AñosFelices
+{
+    public partial class frmRegistrarEmpleado : Form
+    {
+        public frmRegistrarEmpleado()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+      
+
+       
+    }
+}
